aa57f814715a4e9c442b6a4dffe6f7e6ebca50f5
Josh Young <18503267+joshayoung@users.noreply.github.com>
2026-03-07 13:25:25 -0600

    Change the background color

--- app/src/main/java/com/plcoding/coroutinesmasterclass/util/PhotoProcessor.kt
+++ app/src/main/java/com/plcoding/coroutinesmasterclass/util/PhotoProcessor.kt
@@ -1,19 +1,23 @@
 package com.plcoding.coroutinesmasterclass.util
 
 import android.graphics.Bitmap
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 object PhotoProcessor {
-    fun findDominantColor(image: Bitmap): Int {
-        val colorCounts = mutableMapOf<Int, Int>()
-        for (x in 0 until image.width) {
-            for (y in 0 until image.height) {
-                val pixelColor = image.getPixel(x, y)
-                colorCounts[pixelColor] = colorCounts.getOrDefault(pixelColor, 0) + 1
+    suspend fun findDominantColor(image: Bitmap): Int {
+        return withContext(Dispatchers.Default) {
+            val colorCounts = mutableMapOf<Int, Int>()
+            for (x in 0 until image.width) {
+                for (y in 0 until image.height) {
+                    val pixelColor = image.getPixel(x, y)
+                    colorCounts[pixelColor] = colorCounts.getOrDefault(pixelColor, 0) + 1
+                }
             }
+            colorCounts.entries.sortedByDescending { it.value }
+                .take(1)
+                .map { it.key }
+                .first()
         }
-        return colorCounts.entries.sortedByDescending { it.value }
-            .take(1)
-            .map { it.key }
-            .first()
     }
 }
